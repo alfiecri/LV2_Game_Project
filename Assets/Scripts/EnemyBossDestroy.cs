fa939df567c68446427d9e08d5cae9c60280c60d
alfiecri <jediknightalfie@gmail.com>
2021-08-03 04:02:30 +0800

    Update1

---
+++ Assets/Scripts/EnemyBossDestroy.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBossDestroy : MonoBehaviour
+{
+    int bulletsAbsorbed = 12;
+    int health = 25;
+    int healSelf = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag==("Bullet"))
+        {
+            bulletsAbsorbed--;
+            if (bulletsAbsorbed <= 0)
+            {
+                if (health <=15)
+                {
+                    healSelf++;
+                }
+
+                if (healSelf == 1)
+                {
+                    health = 25;
+                }
+
+                if (health <=0)
+                {
+
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/EnemyThrow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyThrow : MonoBehaviour
+{
+    private float timeBtwShots;
+    public float startTimeBtwShots;
+
+    public GameObject projectile;
+    public Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeBtwShots <=)
+        {
+            Instantiate(projectile, transform.position, Quaternion.identity);
+            timeBtwShots = startTimeBtwShots;
+        }
+        else
+        {
+            timeBtwShots -= Time.deltaTime;
+        }
+    }
+}
